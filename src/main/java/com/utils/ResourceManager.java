e40f134c99811c2bdc58c1cb5bdf947552402017
switchYello <chaoyuynl@foxmail.com>
2019-11-25 12:10:39 +0800

    采用classload或file的形式读取配置文件

--- src/main/java/com/utils/ResourceManager.java
+++ src/main/java/com/utils/ResourceManager.java
@@ -1,15 +1,27 @@
 package com.utils;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.InputStream;
-import java.net.URL;
 
 /*
  * 读取资源文件
  * */
 public class ResourceManager {
 
-    public static InputStream gerResourceForFile(String fileName) {
-        return ClassLoader.getSystemClassLoader().getResourceAsStream(fileName);
+    public static InputStream gerResourceForFile(String fileName) throws FileNotFoundException {
+
+        InputStream in = null;
+        ClassLoader classLoader = ResourceManager.class.getClassLoader();
+        ClassLoader loader = classLoader == null ? ClassLoader.getSystemClassLoader() : classLoader;
+
+        if (loader != null) {
+            in = loader.getResourceAsStream(fileName);
+        }
+        if (in == null) {
+            in = new FileInputStream(fileName);
+        }
+        return in;
     }
 
 }

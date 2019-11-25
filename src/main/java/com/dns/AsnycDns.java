d69785eb1dbf9d439970e77799c3a0674db263a8
switchYello <chaoyuynl@foxmail.com>
2019-11-25 12:07:02 +0800

    dns 改成8.8.8.8

--- src/main/java/com/dns/AsnycDns.java
+++ src/main/java/com/dns/AsnycDns.java
@@ -4,6 +4,7 @@ import io.netty.channel.EventLoop;
 import io.netty.channel.socket.nio.NioDatagramChannel;
 import io.netty.resolver.AddressResolver;
 import io.netty.resolver.AddressResolverGroup;
+import io.netty.resolver.ResolvedAddressTypes;
 import io.netty.resolver.dns.DnsNameResolver;
 import io.netty.resolver.dns.DnsNameResolverBuilder;
 import io.netty.resolver.dns.DnsServerAddressStreamProvider;
@@ -17,33 +18,34 @@ import java.net.InetSocketAddress;
  */
 public class AsnycDns extends AddressResolverGroup {
 
-	public static AsnycDns INSTANCE = new AsnycDns();
+    public static AsnycDns INSTANCE = new AsnycDns();
 
-	@Override
-	protected AddressResolver newResolver(EventExecutor executor) {
-		assertTrue(EventLoop.class.isAssignableFrom(executor.getClass()), "异步dns EventExecutor 转 EventLoop 类型不匹配");
+    @Override
+    protected AddressResolver newResolver(EventExecutor executor) {
+        assertTrue(EventLoop.class.isAssignableFrom(executor.getClass()), "异步dns EventExecutor 转 EventLoop 类型不匹配");
 
-		return getResolver0((EventLoop) executor).asAddressResolver();
-	}
+        return getResolver0((EventLoop) executor).asAddressResolver();
+    }
 
-	private DnsNameResolver getResolver0(EventLoop eventLoop) {
-		return new DnsNameResolverBuilder(eventLoop)
-				.channelType(NioDatagramChannel.class)
-				.maxQueriesPerResolve(1)
-				.optResourceEnabled(false)
-				.ndots(1)
-				.nameServerProvider(pro())
-				.build();
-	}
+    private DnsNameResolver getResolver0(EventLoop eventLoop) {
+        return new DnsNameResolverBuilder(eventLoop)
+                .channelType(NioDatagramChannel.class)
+                .maxQueriesPerResolve(1)
+                .optResourceEnabled(false)
+                .ndots(1)
+                .nameServerProvider(pro())
+                .resolvedAddressTypes(ResolvedAddressTypes.IPV4_PREFERRED)
+                .build();
+    }
 
-	private DnsServerAddressStreamProvider pro() {
-		return new SingletonDnsServerAddressStreamProvider(new InetSocketAddress("114.114.114.114", 53));
-	}
+    private DnsServerAddressStreamProvider pro() {
+        return new SingletonDnsServerAddressStreamProvider(new InetSocketAddress("8.8.8.8", 53));
+    }
 
-	private void assertTrue(boolean instance, String msg) {
-		if (!instance) {
-			throw new RuntimeException(msg);
-		}
-	}
+    private void assertTrue(boolean instance, String msg) {
+        if (!instance) {
+            throw new RuntimeException(msg);
+        }
+    }
 
 }

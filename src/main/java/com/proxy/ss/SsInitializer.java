d2b43dcc8b6fffd869de60127ac783e30aed9133
switchYello <chaoyuynl@foxmail.com>
2019-11-26 11:14:08 +0800

    非debug情况下不添加loggerhandler

--- src/main/java/com/proxy/ss/SsInitializer.java
+++ src/main/java/com/proxy/ss/SsInitializer.java
@@ -8,8 +8,12 @@ import io.netty.channel.Channel;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelPipeline;
 import io.netty.handler.logging.LoggingHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class SsInitializer extends ChannelInitializer<Channel> {
+    private static Logger log = LoggerFactory.getLogger(SsInitHandler.class);
+    private static boolean isDebug = log.isDebugEnabled();
 
     @Override
     protected void initChannel(Channel ch) {
@@ -18,9 +22,13 @@ public class SsInitializer extends ChannelInitializer<Channel> {
         //连接超时
         p.addLast(new IdleStateHandlerImpl(30, 30, 0));
         //加密解密方式
-        p.addLast(new LoggingHandler("ss客户端请求流 密文"));
+        if (isDebug) {
+            p.addLast(new LoggingHandler("ss客户端请求流 密文"));
+        }
         p.addLast(Encrypt.get(conf.getEncrypt()));
-        p.addLast(new LoggingHandler("ss客户端请求流明文"));
+        if (isDebug) {
+            p.addLast(new LoggingHandler("ss客户端请求流明文"));
+        }
         p.addLast(new SsInitHandler());
         p.addLast(new SsServiceHandler());
     }
